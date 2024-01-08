41577867b9c506c890692c847bc37d2d897d3410
laurentlfa <138077344+laurentlfa@users.noreply.github.com>
2024-01-08 12:13:22 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # iniciante-na-programacao
+
 Criei este repositório para começar minha aventura no meu pedaço da internet.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Estou dando mais um passo para a construção do meu pedaço de internet. Só estamos no começo!</h1>
+  return (
+    <h1>
+      Estou dando mais um passo para a construção do meu pedaço de internet. Só
+      estamos no começo!
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
