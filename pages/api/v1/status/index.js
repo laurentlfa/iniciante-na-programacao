0fabdd8d39c7b91d78c45a69a92eb0909112de62
laurentlfa <138077344+laurentlfa@users.noreply.github.com>
2024-01-27 15:21:54 +0000

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response
     .status(200)
     .json({ chave: "Nois Auto Freios a melhor oficina do Brasil" });
